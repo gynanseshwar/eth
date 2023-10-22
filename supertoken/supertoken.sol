d3f2515567636e5025f4e28d4ee0d5c59f59361c
gynanseshwar <148713548+gynanseshwar@users.noreply.github.com>
2023-10-22 23:19:30 +0530

    Update supertoken.sol

--- supertoken/supertoken.sol
+++ supertoken/supertoken.sol
@@ -1,1 +1,29 @@
 
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.18;
+
+contract SuperToken {
+  string public tokenName;
+  uint256 public totalTokens;
+
+  mapping(address => uint256) public tokenHoldings;
+
+  constructor(string memory _name, uint256 _initialTokens) {
+    tokenName = _name;
+    totalTokens = _initialTokens;
+    tokenHoldings[msg.sender] = _initialTokens;
+  }
+
+  function Mint (address _receiver, uint256 _amount) public {
+    require(_amount > 0, "Amount must be greater than zero");
+    totalTokens += _amount;
+    tokenHoldings[_receiver] += _amount;
+  }
+
+  function  Burn (uint256 _amount) public {
+    require(_amount > 0, "Amount must be greater than zero");
+    require(tokenHoldings[msg.sender] >= _amount, "Insufficient token holdings");
+    totalTokens -= _amount;
+    tokenHoldings[msg.sender] -= _amount;
+  }
+}
